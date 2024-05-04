3ef31c9816042fbb603222d2673778a296b45a72
Kishore <mkishore207@gmail.com>
2024-05-04 12:09:12 +0530

    first commit

---
+++ src/main/java/TestNG_Maven/TestNG_Giri/App.java
@@ -0,0 +1,13 @@
+package TestNG_Giri.TestNG_Giri;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
